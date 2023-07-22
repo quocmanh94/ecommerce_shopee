cae8c7a37d606c198c74047b2533ef14040492eb
quocmanh94 <quocmanhdang94@gmail.com>
2023-07-22 10:18:49 +0700

    Initialize project

---
+++ src/main/java/com/ecommerce/shopee/EcommerceShopeeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceShopeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceShopeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/shopee/EcommerceShopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
